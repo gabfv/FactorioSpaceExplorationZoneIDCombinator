ec4f461c2df86c2aeddfd76ce1900643f9da70f6
Wiwiweb <wiwiweb2@gmail.com>
2022-09-01 19:32:57 -0700

    Final scripting touches

--- control.lua
+++ control.lua
@@ -1,39 +1,64 @@
+local CME_INFO_CACHE_REFRESH_RATE = 3600
 local util = require("__core__/lualib/util.lua")
 
 local s_signal = {type = "virtual", name = "signal-S"}
 local w_signal = {type = "virtual", name = "signal-W"}
-local function set_combinator_values(entity, seconds_value, megawatts_value)
+local j_signal = {type = "virtual", name = "signal-J"}
+local function set_combinator_values(entity, values)
   local control_behavior = entity.get_control_behavior()
-  control_behavior.set_signal(1, {signal = s_signal, count = seconds_value})
-  control_behavior.set_signal(2, {signal = w_signal, count = megawatts_value})
+  control_behavior.set_signal(1, {signal = s_signal, count = values[1]})
+  control_behavior.set_signal(2, {signal = w_signal, count = values[2]})
+  control_behavior.set_signal(3, {signal = j_signal, count = values[3]})
 end
 
 local function unset_combinator_values(entity)
   local control_behavior = entity.get_control_behavior()
   control_behavior.set_signal(1, nil)
   control_behavior.set_signal(2, nil)
+  control_behavior.set_signal(3, nil)
 end
 
-local function process_surface(surface_name)
-  for _, combinator in pairs(global.combinators_by_surface[surface_name]) do
-    set_combinator_values(combinator, 69, 420)
+local function update_combinators_on_surface(surface_name)
+  local surface_cme_info = global.cme_info[surface_name][1] -- Let's just assume there's only 1 CME per surface for performance
+  if surface_cme_info then
+    local values = {
+      (surface_cme_info.tick - game.tick) / 60, -- S: seconds
+      surface_cme_info.peak_power / 1000000, -- W: MegaWatts
+      surface_cme_info.energy / 1000000, -- J: MegaJoules
+    }
+    for _, combinator in pairs(global.combinators_by_surface[surface_name]) do
+      -- set_combinator_values(combinator, values)
+      set_combinator_values(combinator, values)
+    end
+  else
+    for _, combinator in pairs(global.combinators_by_surface[surface_name]) do
+      unset_combinator_values(combinator)
+    end
   end
 end
 
 local function on_tick_less_than_60_surfaces(event)
   local surface_name = global.surface_list[event.tick % 60] -- surface #1 processed on tick 1, surface #2 on tick 2, etc.
   if surface_name then
-    process_surface(surface_name)
+    update_combinators_on_surface(surface_name)
   end
 end
 
 local function on_tick_more_than_60_surfaces(event)
-
+  for i = event.tick % 60, #global.surface_list, 60 do
+    update_combinators_on_surface(global.surface_list[i])
+  end
 end
+
+local function refresh_cme_info()
+  global.cme_info = remote.call("space-exploration", "get_solar_flares")
+end
+script.on_nth_tick(CME_INFO_CACHE_REFRESH_RATE, refresh_cme_info)
 
 local function on_init()
   global.combinators_by_surface = {}
   global.surface_list = {} -- Used to split work by surface, surface #1 processed on tick 1, surface #2 on tick 2, etc.
+  refresh_cme_info()
   script.on_event(defines.events.on_tick, on_tick_less_than_60_surfaces)
 end
 script.on_init(on_init)
@@ -69,10 +94,12 @@ end
 
 local function on_entity_created(event)
   local entity = event.created_entity or event.entity
-  if not global.combinators_by_surface[entity.surface.name] then
-    add_new_surface(entity.surface.name)
+  local surface_name = entity.surface.name
+  if surface_name == "nauvis" then surface_name = "Nauvis" end
+  if not global.combinators_by_surface[surface_name] then
+    add_new_surface(surface_name)
   end
-  table.insert(global.combinators_by_surface[entity.surface.name], entity)
+  table.insert(global.combinators_by_surface[surface_name], entity)
   log(serpent.line(global.combinators_by_surface))
 end
 script.on_event(defines.events.on_robot_built_entity, on_entity_created, entity_filter)
@@ -91,9 +118,11 @@ local function remove_from_list_by_unit_number(table_to_search, entity)
 end
 
 local function on_entity_removed(event)
-  remove_from_list_by_unit_number(global.combinators_by_surface[event.entity.surface.name], event.entity)
-  if #global.combinators_by_surface[event.entity.surface.name] == 0 then
-    remove_surface(event.entity.surface.name)
+  local surface_name = event.entity.surface.name
+  if surface_name == "nauvis" then surface_name = "Nauvis" end
+  remove_from_list_by_unit_number(global.combinators_by_surface[surface_name], event.entity)
+  if #global.combinators_by_surface[surface_name] == 0 then
+    remove_surface(surface_name)
   end
   log(serpent.line(global.combinators_by_surface))
 end
