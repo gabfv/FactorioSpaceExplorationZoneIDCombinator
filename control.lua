ecab01db87914659a01422aa45ec249935895a5b
Wiwiweb <wiwiweb2@gmail.com>
2022-09-03 15:44:28 -0700

    Keep CME info while CME is ongoing
    This lets combinators go to -2 minutes

--- control.lua
+++ control.lua
@@ -1,5 +1,17 @@
-local CME_INFO_CACHE_REFRESH_RATE = 3600
+local CME_INFO_CACHE_REFRESH_RATE = 60*60*1 -- 1 minute
+local CME_DURATION = 60*60*2-- 2 minutes
+local NEGATIVE_SECONDS_BEFORE_REFRESH = -CME_DURATION/60 +1
 local util = require("__core__/lualib/util.lua")
+
+
+local function refresh_cme_info()
+  -- SE deletes the CME from the list as soon as it starts.
+  -- Block refreshes while a CME is ongoing to keep combinators on during a CME.
+  if not global.ongoing_cme then
+    global.cme_info = remote.call("space-exploration", "get_solar_flares")
+  end
+end
+script.on_nth_tick(CME_INFO_CACHE_REFRESH_RATE, refresh_cme_info)
 
 local s_signal = {type = "virtual", name = "signal-S"}
 local w_signal = {type = "virtual", name = "signal-W"}
@@ -19,10 +31,20 @@ local function unset_combinator_values(entity)
 end
 
 local function update_combinators_on_surface(surface_name, current_tick)
-  local surface_cme_info = global.cme_info[surface_name][1] -- Let's just assume there's only 1 CME per surface for performance
+  local surface_cme_info = global.cme_info[surface_name]
   if surface_cme_info then
+    surface_cme_info = surface_cme_info[1] -- Let's just assume there's only 1 CME per surface for performance
+
+    local seconds = (surface_cme_info.tick - current_tick) / 60
+    if NEGATIVE_SECONDS_BEFORE_REFRESH < seconds and seconds <= 1 then
+      global.ongoing_cme = true -- ongoing CME
+    elseif seconds <= NEGATIVE_SECONDS_BEFORE_REFRESH then
+      global.ongoing_cme = false -- CME has passed
+      refresh_cme_info()
+    end
+
     local values = {
-      (surface_cme_info.tick - current_tick) / 60, -- S: seconds
+      seconds, -- S: seconds
       surface_cme_info.peak_power / 1000000, -- W: MegaWatts
       surface_cme_info.energy / 1000000, -- J: MegaJoules
     }
@@ -50,14 +72,10 @@ local function on_tick_more_than_60_surfaces(event)
   end
 end
 
-local function refresh_cme_info()
-  global.cme_info = remote.call("space-exploration", "get_solar_flares")
-end
-script.on_nth_tick(CME_INFO_CACHE_REFRESH_RATE, refresh_cme_info)
-
 local function on_init()
   global.combinators_by_surface = {}
   global.surface_list = {} -- Used to split work by surface, surface #1 processed on tick 1, surface #2 on tick 2, etc.
+  global.ongoing_cme = false
   refresh_cme_info()
   script.on_event(defines.events.on_tick, on_tick_less_than_60_surfaces)
 end
@@ -94,6 +112,7 @@ end
 
 local function on_entity_created(event)
   local entity = event.created_entity or event.entity
+  entity.operable = false
   local surface_name = entity.surface.name
   if surface_name == "nauvis" then surface_name = "Nauvis" end
   if not global.combinators_by_surface[surface_name] then
